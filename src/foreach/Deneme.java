e6f90fdf734737b6193c7c18af44f8ca8d3a8d06
lab-001 <okankgz@gmail.com>
2019-03-28 14:38:44 +0300

    Initial commit

---
+++ src/foreach/Deneme.java
@@ -0,0 +1,17 @@
+
+import java.io.Serializable;
+
+
+public class Deneme {
+    private String isim;
+
+    public Deneme(String isim) {
+        this.isim = isim;
+    }
+    public void yaz() {
+
+        System.out.println(isim);
+    }
+
+
+}
